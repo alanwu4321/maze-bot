ba3a6cf9628d986ea0b8a699bf24c0a3c9c45257
Alan Wu <ac2wu@edu.uwaterloo.ca>
2020-03-05 22:53:07 -0500

    bestBuy and walmart API

--- server.js
+++ server.js
@@ -3,33 +3,53 @@ const express = require('express')
 const scraper = require('./utils/scraper')
 const app = express()
 // app.set('view engine', 'pug')
-const handlers = {"amazon": scraper.scrapeAmazon, "staples": scraper.scrapeStaples}
+const handlers = {
+  "amazon": scraper.scrapeAmazon,
+  "staples": scraper.scrapeStaples,
+  "walmart": scraper.scrapeWalmart,
+  "bestbuy": scraper.scrapeBestBuy,
+}
 
 app.get('/', (req, res) => {
   console.log(req.query)
-  stores = req.query["stores"].split(",")
+  let stores = []
+  if(req.query['stores'] != null){
+    stores = req.query.stores.split(",")
+  }else{
+    stores = ["amazon", "staples","walmart", "bestbuy"]
+  }
+  let keywords = req.query.keywords.split(",")
   promises = []
   stores.forEach(store => {
     console.log("Starting to scrape store " + store)
     promises.push(new Promise((resolve, reject) => {
       scraper
-        .handleStore(handlers[store], req.query)
+        .handleStore(handlers[store], keywords)
         .then(data => {
           console.log("Finsihed scraping store " + store)
           console.log(data)
-          resolve({[store]:data})
+          resolve({ status: "success", [store]: data })
         })
         .catch(err => {
-          reject(`${store} scrape failed: ` +  err)
+          console.log(err)
+          resolve({ status: "error", [store]: err })
         })
     }))
   });
 
   Promise.all(promises)
     .then(data => {
-      res.status(200).send({res: data})
+      res.status(200)
+        .send(
+          {
+            length: data.length,
+            stores: stores,
+            keywords: keywords,
+            response: data
+          }
+        )
     })
-    .catch(err => res.status(500).send({error:err}))
+  // .catch(err => res.status(500).send({error:err}))
 })
 
 app.listen(process.env.PORT || 3000)
--- utils/scraper.js
+++ utils/scraper.js
@@ -1,14 +1,15 @@
 const puppeteer = require('puppeteer')
-const handleStore = async (handler, query) => {
+const axios = require('axios');
+
+const handleStore = async (handler, keywords) => {
   return new Promise(function (resolve, reject) {
-    let keywords = query.keywords.split(",")
     console.log(keywords)
     let counter = 0
     let res = []
     keywords.forEach((keyword) => {
       handler(keyword).then(
         data => {
-          res.push({[keyword]: data})
+          res.push({ [keyword]: data })
           counter += 1
           if (counter == keywords.length) {
             resolve(res)
@@ -21,10 +22,33 @@ const handleStore = async (handler, query) => {
   })
 }
 
+const scrapeBestBuy = (keyword) => {
+  return new Promise(function (resolve, reject) {
+    let prod = {
+      name: null,
+      url: null,
+      uuid: null,
+      salePrice: null,
+      regularPrice: null,
+    }
+    axios.get(`https://api.bestbuy.com/v1/products((search=${keyword}))?apiKey=tda21Z9pyCFomCJM211gkCrY&sort=modelNumber.asc&show=modelNumber,name,salePrice,regularPrice&pageSize=100&format=json`)
+      .then(response => {
+        if (response.data["products"].length > 0){
+          resolve(response.data["products"][0])
+        }
+        resolve(prod)
+      })
+      .catch(error => {
+        reject(error)
+      });
+  })
+}
+
 const scrapeAmazon = async (keyword) => {
   const browser = await puppeteer.launch({
     headless: true, args: [
       '--window-size=1000,1000',
+      '--no-sandbox', '--disable-setuid-sandbox'
     ]
   })
   const page = await browser.newPage()
@@ -80,6 +104,7 @@ const scrapeStaples = async (keyword) => {
   const browser = await puppeteer.launch({
     headless: true, args: [
       '--window-size=1000,1000',
+      '--no-sandbox', '--disable-setuid-sandbox'
     ]
   })
   const page = await browser.newPage()
@@ -88,7 +113,7 @@ const scrapeStaples = async (keyword) => {
   await page.goto(
     `https://www.staples.ca/search?query=${keyword}`
   )
-  
+
   const scrapedData = await page.evaluate(() => {
     const numberify = string => Number(string.replace(/[^\d.]+/, '').replace(/\btp\./g, '').replace(',', '') || 0)
     const products = Array.from(document.querySelectorAll('.ais-hits--item'))
@@ -101,12 +126,12 @@ const scrapeStaples = async (keyword) => {
           regularPrice: null,
         }
         title = product.querySelector("a.product-thumbnail__title.product-link")
-        if(title != null){
+        if (title != null) {
           prod.name = title.textContent
           prod.url = "https://www.staples.ca" + title.getAttribute("href")
         }
         price = product.querySelector(".money.pre-money")
-        if(price != null){
+        if (price != null) {
           prod.salePrice = numberify(price.textContent)
           prod.uuid = price.getAttribute("data-product-id")
         }
@@ -114,12 +139,82 @@ const scrapeStaples = async (keyword) => {
       }).filter(product => {
         return product.name != null && (product.salePrice != null || product.regularPrice != null);
       })
-      return products[0]
-    })
+    return products[0]
+  })
   await browser.close()
   return scrapedData
 }
 
+const scrapeWalmart = async (keyword) => {
+  const browser = await puppeteer.launch({
+    headless: true, args: [
+      '--window-size=1000,1000',
+      '--no-sandbox', '--disable-setuid-sandbox'
+    ]
+  })
+  const page = await browser.newPage()
+  await page.setViewport({ width: 1000, height: 1000, deviceScaleFactor: 1 })
+  await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
+  await page.goto(
+    `https://www.walmart.ca/search/${keyword}`
+  )
+  const scrapedData = await page.evaluate(() => {
+    const numberify = string => Number(string.replace(/[^\d.]+/, '').replace(/\btp\./g, '').replace(',', '') || 0)
+    const products = Array.from(document.querySelectorAll('.product-link'))
+      .map(product => {
+        let prod = {
+          name: null,
+          url: null,
+          uuid: null,
+          salePrice: null,
+          regularPrice: null,
+        }
+        prod.url = "https://www.walmart.ca" + product.getAttribute("href")
+        uuid = prod.url.split("/")
+        if (uuid.length > 0) {
+          prod.uuid = uuid[uuid.length - 1]
+        }
+        title = product.querySelector(".thumb-header")
+        if (title != null) {
+          prod.name = title.textContent
+        }
+
+        price = product.querySelector("span.product-price-analytics")
+        if (price != null) {
+          prod.salePrice = numberify(price.getAttribute("data-analytics-value"))
+          if (prod.salePrice == 0) {
+            prod.salePrice = -1
+          }
+        } else {
+          curPrice = product.querySelector(".price-current")
+          if (curPrice != null) {
+            prod.salePrice = numberify(curPrice.textContent)
+          }
+        }
+        price_was = product.querySelector(".price-was")
+        if (price_was != null) {
+          prod.regularPrice = numberify(price_was.textContent)
+          if (prod.regularPrice == 0) {
+            prod.regularPrice = null
+          }
+        }
+        return prod
+      }).filter(product => {
+        return product.name != null && (product.salePrice != null || product.regularPrice != null);
+      })
+    return products[0]
+  })
+  await browser.close()
+  return scrapedData
+}
+
+
+
 module.exports.handleStore = handleStore
 module.exports.scrapeAmazon = scrapeAmazon
 module.exports.scrapeStaples = scrapeStaples
+module.exports.scrapeWalmart = scrapeWalmart
+module.exports.scrapeBestBuy = scrapeBestBuy
+
+
+
