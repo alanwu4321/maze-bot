f62da0d6541bac173c1a4af7981450cdeb68667a
Alan Wu <ac2wu@edu.uwaterloo.ca>
2020-03-30 02:32:03 -0700

    update clean up script

--- clean.sh
+++ clean.sh
@@ -1,1 +1,1 @@
-tmux kill-session -t hi & zookeeper-server-stop /usr/local/etc/kafka/zookeeper.properties & kafka-server-stop /usr/local/etc/kafka/server.properties & brew services stop redis
+tmux kill-session -t mazeBot & zookeeper-server-stop /usr/local/etc/kafka/zookeeper.properties & kafka-server-stop /usr/local/etc/kafka/server.properties & brew services stop redis
